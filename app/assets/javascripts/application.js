c84cb2afbe911f9f96189dbd9978ad859ab1eb9d
Deivide (David) Oliveira <deivide.s.oliveira@gmail.com>
2015-07-05 05:41:31 +0000

    Add better CSS design to GMaps infoWindow

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,11 +30,19 @@ $(document).ready(function() {
         var pos = new google.maps.LatLng(position.coords.latitude,
                                          position.coords.longitude);
 
-        var infowindow = new google.maps.InfoWindow({
-          map: map,
+        var userMarker = new google.maps.Marker({
           position: pos,
-          content: 'You are here.'
+          map: map,
+          title: 'You are here!',
+          icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+          animation: google.maps.Animation.DROP
         });
+
+        // var userInfoWindow = new google.maps.InfoWindow({
+        //   content: "You are here!"
+        // });
+
+        // addInfoWindowListener(userMarker, userInfoWindow);
 
         map.setCenter(pos);
       }, function() {
@@ -56,30 +64,100 @@ $(document).ready(function() {
       map.setCenter(options.position);
     }
 
-    // Load centres from the database and set each pin data
+    // Load C. Centres from the database and set each pin data
     var pins = [];
     $.get('/centres.json').done(function(data) {
       pins = data
       $.each(pins, function(index, item){
-        addPin(item.latitude, item.longitude, item.name, item.description);
+        addPin(
+          item.latitude, 
+          item.longitude, 
+          item.name, 
+          item.description,
+          item.address,
+          item.phone,
+          item.email,
+          item.city,
+          item.province
+        );
       });
     });
 
-    var addPin = function(lat, long, name, description) {
+    // Add all Pins to the map on the specified position
+    var addPin = function(lat, long, name, description, address, phone, email, city, province) {
       var loc = new google.maps.LatLng(lat, long);
+      // Create new marker
       var newMarker = new google.maps.Marker({
         position: loc,
         map: map,
         title: name,
-        icon: 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png'
+        icon: 'http://maps.google.com/mapfiles/ms/icons/pink-dot.png',
+        animation: google.maps.Animation.DROP
       });
 
+      //Set InfoWindow properties
       var newInfoWindow = new google.maps.InfoWindow({
-        content: "<h3>Description: " + description + "</h3>"
+        content: '<div id="iw-container">' +
+                    '<div class="iw-title">' + name + '</div>' +
+                    '<div class="iw-content">' +
+                      '<p>' + description + '</p>' +
+                      '<div class="iw-subTitle">Contacts</div>' +
+                      '<p>Phone: ' + phone + '<br>Email: ' + email + '<br>'+
+                      '<p>Address: ' + address + ', ' + city + ' - ' + province + '<br>' +
+                    '</div>' +
+                    '<div class="iw-bottom-gradient"></div>' +
+                  '</div>',
+        maxWidth: 350
       });
+
       addInfoWindowListener(newMarker, newInfoWindow);
+
+      google.maps.event.addListener(newInfoWindow, 'domready', function() {
+
+        // Reference to the DIV that wraps the bottom of infowindow
+        var iwOuter = $('.gm-style-iw');
+
+        /* Since this div is in a position prior to .gm-div style-iw.
+         * We use jQuery and create a iwBackground variable,
+         * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
+        */
+        var iwBackground = iwOuter.prev();
+
+        // Removes background shadow DIV
+        iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+        // Removes white background DIV
+        iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+        // Moves the infowindow 115px to the right.
+        iwOuter.parent().parent().css({left: '115px'});
+
+        // Moves the shadow of the arrow 76px to the left margin.
+        iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+        // Moves the arrow 76px to the left margin.
+        iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+        // Changes the desired tail shadow color.
+        iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
+
+        // Reference to the div that groups the close button
+        var iwCloseBtn = iwOuter.next();
+        iwCloseBtn.css({opacity: '1', right: '60px', top: '20px'});
+
+        // If the content of infowindow not exceed the set maximum height, then the gradient is removed.
+        if($('.iw-content').height() < 140){
+          $('.iw-bottom-gradient').css({display: 'none'});
+        }
+
+        // The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
+        iwCloseBtn.mouseout(function(){
+          $(this).css({opacity: '1'});
+        });
+      });
     }
 
+    //Function to toggle InfoWindow On/Off based on user's click
     var lastInfoWindow;
     var addInfoWindowListener = function(marker, newInfoWindow) {
       google.maps.event.addListener(marker, 'click', function() {
@@ -94,51 +172,12 @@ $(document).ready(function() {
           lastInfoWindow = newInfoWindow;
         }
       });
+      // Closes infoWindow if user clicks anywhere on the map
+      google.maps.event.addListener(map, 'click', function() {
+        newInfoWindow.close();
+      });      
     }
 
-    // var contentString = '<div id="content">'+
-    //     '<div id="siteNotice">'+
-    //     '</div>'+
-    //     '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-    //     '<div id="bodyContent">'+
-    //     '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    //     'sandstone rock formation in the southern part of the '+
-    //     'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-    //     'south west of the nearest large town, Alice Springs; 450&#160;km '+
-    //     '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-    //     'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-    //     'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-    //     'Aboriginal people of the area. It has many springs, waterholes, '+
-    //     'rock caves and ancient paintings. Uluru is listed as a World '+
-    //     'Heritage Site.</p>'+
-    //     '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-    //     'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-    //     '(last visited June 22, 2009).</p>'+
-    //     '</div>'+
-    //     '</div>';
-
-    // var infowindow = new google.maps.InfoWindow({
-    //     content: contentString
-    // });
-
-    // var theirMarker = new google.maps.Marker({
-    //     position: theirLatLng,
-    //     map: map,
-    //     title: 'Hello World!'
-    // });
-
-    // var myMarker = new google.maps.Marker({
-    //     position: myLatLng,
-    //     map: map,
-    //     draggable:true,
-    //     animation: google.maps.Animation.DROP,
-    //     title: 'Deer Lake!'
-    // });
-
-    // google.maps.event.addListener(newMarker, 'click', function() {
-    //   infowindow.open(map,this);
-    // });
   }
-
   google.maps.event.addDomListener(window, 'load', initialize);
 });
